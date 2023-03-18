674a6083d8833e2e8e56cc66b777bed8cc90eaf4
Liza Stepanova <tuchafeb@mail.ru>
2023-03-18 16:45:14 +0300

    git commit -m "pom.xml"

--- src/test/java/ru/netology/java/qa/PosterManagerTest.java
+++ src/test/java/ru/netology/java/qa/PosterManagerTest.java
@@ -132,6 +132,27 @@ public class PosterManagerTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+    @Test
+    public void IfLimit10() {
+        PosterManager manager = new PosterManager();
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+        manager.add("Film 7");
+        manager.add("Film 8");
+        manager.add("Film 9");
+        manager.add("Film 10");
+
+
+        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 
 
 
